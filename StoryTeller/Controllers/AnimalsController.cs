a1d1bfef2312f7e3266e9eba3cc587a386af68e1
malloryrbush <malloryrbush@hotmail.com>
2018-08-07 15:25:36 -0400

    code first

---
+++ StoryTeller/Controllers/AnimalsController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StoryTeller;
+using Microsoft.AspNet.Identity;
+using StoryTeller.Models;
+using StoryTeller.Data;
+
+namespace StoryTeller.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private StoryTellerContext db = new StoryTellerContext();
+
+        // GET: Animals
+        public ActionResult Index()
+        {
+            return View(db.Animals.ToList());
+        }
+
+        // GET: Animals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animal animal = db.Animals.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        // GET: Animals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AnimalID,Text,UserInput")] Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Animals.Add(animal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(animal);
+        }
+
+        // GET: Animals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animal animal = db.Animals.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            var avw = new AnimalViewModel();
+            avw.AnimalID = animal.Id;
+            avw.Text = animal.Text;
+            return View(avw);
+        }
+
+        // POST: Animals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AnimalID,Text,UserInput")] AnimalViewModel avw)
+        {
+            var username = HttpContext.User.Identity.GetUserName();
+
+
+            Animal animal = db.Animals.Find(avw.AnimalID);
+            animal.Id = avw.AnimalID;
+            animal.Text += "\n \n" + avw.UserInput;
+
+            if (ModelState.IsValid)
+            {
+                animal.Author = username;
+
+                db.Entry(animal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(animal.Text);
+        }
+
+        // GET: Animals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animal animal = db.Animals.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        // POST: Animals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Animal animal = db.Animals.Find(id);
+            db.Animals.Remove(animal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+
+}
+
