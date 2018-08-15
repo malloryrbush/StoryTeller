263e1fb65fb3124275117b55f979f64414059ae5
malloryrbush <malloryrbush@hotmail.com>
2018-08-15 10:50:41 -0400

    styling changes

--- StoryTeller/Scripts/JavaScript1.js
+++ StoryTeller/Scripts/JavaScript1.js
@@ -18,3 +18,30 @@ function myFunction(xhttp) {
 function ruleAlert() {
     alert("RULES FOR POSTING:    1. NO SWEARING.       2. HAVE FUN.");
 }
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
